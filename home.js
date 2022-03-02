7bacd485e03b5f8b054a0006bbece1df447c8776
SURESHA8762402534 <70739688+SURESHA8762402534@users.noreply.github.com>
2022-03-02 11:47:35 +0530

    Update home.js

--- home.js
+++ home.js
@@ -6,7 +6,7 @@ class Home extends React.Component{
                 <span id="first">
                     <h1>I   AM   SURESHA   B   M</h1>
                     <h2>Aspiring Techie</h2>
-                    <h4>I am a hardworking engineering graduate, who have interests in exploring<br></br>new things and want to work in the field of Web Development. </h4>
+                    <h4>I am a hardworking engineering graduate, who have interests in exploring new things and want to work in the field of Web Development. </h4>
                     <button>About Me</button>
                 </span>
                 <span id="photo">
